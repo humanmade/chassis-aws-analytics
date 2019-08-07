f5b883110d82044abbd603727f233d6773ae00b5
Robert O'Rourke <robert@o-rourke.org>
2019-08-07 13:18:07 +0100

    split out snalytics_service

--- modules/chassis-aws-analytics/manifests/init.pp
+++ modules/chassis-aws-analytics/manifests/init.pp
@@ -18,50 +18,6 @@ class chassis-aws-analytics (
 		mode => '0755',
 	}
 
-	# Create a service resource.
-	define analytics_service (
-		$status = running,
-		$active = true
-	) {
-		# Install.
-		exec { "${name} install":
-			command => '/usr/bin/npm install --production',
-			cwd     => "/vagrant/extensions/chassis-aws-analytics/local-${name}",
-			user    => 'vagrant',
-			unless  => "/usr/bin/test -d /vagrant/extensions/chassis-aws-analytics/local-${name}/node_modules",
-			require => [
-				Package['nodejs'],
-			],
-		}
-
-		# Enable the service.
-		exec { "systemctl enable ${name}":
-			command     => "/bin/systemctl enable ${name}",
-			refreshonly => true,
-		}
-
-		# Create service file.
-		file { "/lib/systemd/system/${name}.service":
-			ensure  => file,
-			mode    => '0644',
-			content => template("chassis-aws-analytics/${name}.service"),
-			notify  => [
-				Exec["systemctl enable ${name}"],
-				Exec['systemctl-daemon-reload'],
-			],
-		}
-
-		# Run the service.
-		service { $name:
-			ensure    => $status,
-			enable    => $active,
-			restart   => $active,
-			hasstatus => $active,
-			require   => Exec["${name} install"],
-			subscribe => File["/lib/systemd/system/${name}.service"],
-		}
-	}
-
 	$services = [ 'cognito', 'pinpoint' ]
 
 	# Create services.
